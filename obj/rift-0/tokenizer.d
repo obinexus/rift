faa36e38cdf4f87a307d8aec60d45ac4959e715e
Nnamdi Michael Okpala <support@obinexus.org>
2025-06-19 04:20:28 +0100

    work

---
+++ obj/rift-0/tokenizer.d
@@ -0,0 +1,4 @@
+/mnt/c/Users/OBINexus/Projects/github/rift/tools/../obj/rift-0/tokenizer.o: \
+ /mnt/c/Users/OBINexus/Projects/github/rift/tools/../rift/src/core/stage-0/tokenizer.c \
+ /mnt/c/Users/OBINexus/Projects/github/rift/tools/../rift/include/rift/core/common.h
+/mnt/c/Users/OBINexus/Projects/github/rift/tools/../rift/include/rift/core/common.h:
