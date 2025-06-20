ee3c9e587d757724f448d1b6abb0bade203ac30e
Nnamdi Michael Okpala <support@obinexus.org>
2025-06-20 01:29:38 +0100

    broken make

--- obj/stage-4/bytecode.d
+++
@@ -1,4 +0,0 @@
-/mnt/c/Users/OBINexus/Projects/github/rift/obj/stage-4/bytecode.o: \
- /mnt/c/Users/OBINexus/Projects/github/rift/rift/src/core/stage-4/bytecode.c \
- /mnt/c/Users/OBINexus/Projects/github/rift/rift/include/rift/core/common.h
-/mnt/c/Users/OBINexus/Projects/github/rift/rift/include/rift/core/common.h:
--- obj/stage-6/emitter.d
+++
@@ -1,4 +0,0 @@
-/mnt/c/Users/OBINexus/Projects/github/rift/obj/stage-6/emitter.o: \
- /mnt/c/Users/OBINexus/Projects/github/rift/rift/src/core/stage-6/emitter.c \
- /mnt/c/Users/OBINexus/Projects/github/rift/rift/include/rift/core/common.h
-/mnt/c/Users/OBINexus/Projects/github/rift/rift/include/rift/core/common.h:
