9f21541025ab9e4da51b146f5a3f50770fd1f26b
Nnamdi Michael Okpala <support@obinexus.org>
2025-06-20 02:57:15 +0100

    poc/gov

---
+++ poc/gov/rift/src/cli/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(int argc, char *argv[]) {
+    printf("RIFT Unified CLI v1.6.0\n");
+    printf("AEGIS Methodology Compliance: ENABLED\n");
+    printf("Governance Validation: ACTIVE\n");
+    return 0;
+}
