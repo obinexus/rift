e014bc297914adec5894f948d528b924154666ab
Nnamdi Michael Okpala <support@obinexus.org>
2025-06-20 03:57:16 +0100

    W

--- poc/gov/bin/rift.d
+++ poc/gov/bin/rift.d
@@ -1,2 +1,2 @@
 /mnt/c/Users/OBINexus/Projects/github/rift/poc/gov/bin/rift: \
- /mnt/c/Users/OBINexus/Projects/github/rift/poc/gov/rift/src/cli/main.c
+ /mnt/c/Users/OBINexus/Projects/github/rift/poc/gov/cli/main.c
