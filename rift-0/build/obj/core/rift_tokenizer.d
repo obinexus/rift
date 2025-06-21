ccf93c9fda62724b6cc71fb0bb5cbf190bb306e7
Nnamdi Michael Okpala <support@obinexus.org>
2025-06-21 01:18:35 +0100

    string

---
+++ rift-0/build/obj/core/rift_tokenizer.d
@@ -0,0 +1,2 @@
+build/obj/core/rift_tokenizer.o: src/core/rift_tokenizer.c \
+ include/rift-0/core/rift_tokenizer.h
--- rift-0/include/rift-0/core/rift_tokenizer.h
+++ rift-0/include/rift-0/core/rift_tokenizer.h
@@ -16,7 +16,9 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <stddef.h>
