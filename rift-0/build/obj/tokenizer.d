ac25861e09f0f9ccc7fbf5a891512db3bf05164b
Nnamdi Michael Okpala <support@obinexus.org>
2025-06-21 03:07:49 +0100

    W

---
+++ rift-0/build/obj/tokenizer.d
@@ -0,0 +1,4 @@
+build/obj/tokenizer.o: src/core/tokenizer.c \
+ include/rift-0/core/tokenizer.h include/rift-0/core/tokenizer_rules.h
+include/rift-0/core/tokenizer.h:
+include/rift-0/core/tokenizer_rules.h:
